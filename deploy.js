1f6f47fc875612ba85b8af8831df5dcec5f7ee84
Anh Viet <anhviet5121pc@gmail.com>
2021-07-06 17:11:11 +0700

    SmartContract with testing

---
+++ deploy.js
@@ -0,0 +1,22 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const Web3 = require("web3");
+const { interface, bytecode } = require("./compile");
+
+const provider = new HDWalletProvider(
+  "kite small symptom impact bundle dutch happy live burst mask trade marble",
+  "https://rinkeby.infura.io/v3/c55c20def1ce4eceb8dd2c280a5c8e1c"
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log("Attempting to deploy from account", accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
+    .send({ gas: "1000000", from: accounts[0] });
+
+  console.log("Contract deployed to", result.options.address);
+};
+deploy();
